dece481d872e0e9638c3059a0e8fd96ac887139d
João Carlos Ferreira Marques <joaocarlusferrera@gmail.com>
2023-12-01 08:45:54 -0300

    forgot to save that file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.7.22"
+    kotlin("jvm") version "1.9.21"
+    application
 }
 
 repositories {
     mavenCentral()
 }
 
-tasks {
-    sourceSets {
-        main {
-            java.srcDirs("src")
-        }
-    }
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
 
-    wrapper {
-        gradleVersion = "7.6"
+    //implementation("org.jetbrains.kotlinx:multik-core:0.2.0")
+    //implementation("org.jetbrains.kotlinx:multik-default:0.2.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.reflections:reflections:0.10.2")
+    implementation("com.github.ajalt.mordant:mordant:2.2.0")
+    implementation("guru.nidi:graphviz-kotlin:0.18.1")
+    implementation("org.slf4j:slf4j-nop:2.0.9")
+    //implementation("ch.qos.logback", "logback-classic", "1.2.3")
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "17"
+        //languageVersion = "1.9"
+        freeCompilerArgs = listOf("-Xcontext-receivers")
     }
+}
+
+application {
+    mainClass.set("AdventOfCodeKt")
 }
--- src/main/kotlin/utils/Utils.kt
+++ src/main/kotlin/utils/Utils.kt
@@ -20,3 +20,4 @@ fun readInputAsText(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+~
