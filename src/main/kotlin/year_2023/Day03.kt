310b445cc2c832f0356930fcef48a20514a779bc
João Carlos Ferreira Marques <joaocarlusferrera@gmail.com>
2023-12-02 12:14:30 -0300

    preparations day 3

---
+++ src/main/kotlin/year_2023/Day03.kt
@@ -0,0 +1,14 @@
+package year_2023
+
+import Day
+import solve
+
+class Day03 : Day(day = 3, year = 2023, "") {
+}
+
+fun main() {
+    solve<Day02>(offerSubmit = true) {
+        """                       
+        """.trimIndent()()
+    }
+}
