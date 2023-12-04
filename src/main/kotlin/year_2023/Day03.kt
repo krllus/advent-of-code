da2f20874a3831cf918e85895ac92d7452930e76
João Carlos Ferreira Marques <joaocarlusferrera@gmail.com>
2023-12-04 08:43:04 -0300

    p2 not working

--- src/main/kotlin/year_2023/Day03.kt
+++ src/main/kotlin/year_2023/Day03.kt
@@ -24,7 +24,7 @@ class Day03 : Day(day = 3, year = 2023, "") {
                     safeExecute { numberStrAdjacent += inputAsGrid[i + 1][j] }
                     safeExecute { numberStrAdjacent += inputAsGrid[i + 1][j + 1] }
                 } else {
-                    if(numberStrAdjacent.any { c -> !(c.isDigit() || c == '.') }){
+                    if (numberStrAdjacent.any { c -> !(c.isDigit() || c == '.') }) {
                         val number = numberStr.toInt()
                         sum += number
                     }
@@ -37,7 +37,114 @@ class Day03 : Day(day = 3, year = 2023, "") {
         return sum
     }
 
+    override fun part2(): Int {
+        val map = mutableMapOf<Point, MutableList<Int>>()
+        for (lineIndex in input.indices) {
+            val line = "${input[lineIndex]}.".toCharArray()
+            var numberStr = ""
+            for (charIndex in line.indices) {
+                val char = line[charIndex]
+                if (char.isDigit()) numberStr += char
+                else {
+
+
+                    safeExecute {
+                        val adj = inputAsGrid[lineIndex - 1][charIndex - 1]
+                        if (adj == '*') {
+                            val number = numberStr.toInt()
+                            val point = Point(lineIndex, charIndex)
+                            val elements = map[point] ?: mutableListOf()
+                            elements.add(number)
+                            map[point] = elements
+                        }
+
+                    }
+                    safeExecute {
+                        val adj = inputAsGrid[lineIndex - 1][charIndex]
+                        if (adj == '*') {
+                            val number = numberStr.toInt()
+                            val point = Point(lineIndex, charIndex)
+                            val elements = map[point] ?: mutableListOf()
+                            elements.add(number)
+                            map[point] = elements
+                        }
+                    }
+                    safeExecute {
+                        val adj = inputAsGrid[lineIndex - 1][charIndex + 1]
+                        if (adj == '*') {
+                            val number = numberStr.toInt()
+                            val point = Point(lineIndex, charIndex)
+                            val elements = map[point] ?: mutableListOf()
+                            elements.add(number)
+                            map[point] = elements
+                        }
+                    }
+
+                    safeExecute {
+                        val adj = inputAsGrid[lineIndex][charIndex - 1]
+                        if (adj == '*') {
+                            val number = numberStr.toInt()
+                            val point = Point(lineIndex, charIndex)
+                            val elements = map[point] ?: mutableListOf()
+                            elements.add(number)
+                            map[point] = elements
+                        }
+                    }
+                    safeExecute {
+                        val adj = inputAsGrid[lineIndex][charIndex + 1]
+                        if (adj == '*') {
+                            val number = numberStr.toInt()
+                            val point = Point(lineIndex, charIndex)
+                            val elements = map[point] ?: mutableListOf()
+                            elements.add(number)
+                            map[point] = elements
+                        }
+                    }
+
+                    safeExecute {
+                        val adj = inputAsGrid[lineIndex + 1][charIndex - 1]
+                        if (adj == '*') {
+                            val number = numberStr.toInt()
+                            val point = Point(lineIndex, charIndex)
+                            val elements = map[point] ?: mutableListOf()
+                            elements.add(number)
+                            map[point] = elements
+                        }
+                    }
+                    safeExecute {
+                        val adj = inputAsGrid[lineIndex + 1][charIndex]
+                        if (adj == '*') {
+                            val number = numberStr.toInt()
+                            val point = Point(lineIndex, charIndex)
+                            val elements = map[point] ?: mutableListOf()
+                            elements.add(number)
+                            map[point] = elements
+                        }
+                    }
+                    safeExecute {
+                        val adj = inputAsGrid[lineIndex + 1][charIndex + 1]
+                        if (adj == '*') {
+                            val number = numberStr.toInt()
+                            val point = Point(lineIndex, charIndex)
+                            val elements = map[point] ?: mutableListOf()
+                            elements.add(number)
+                            map[point] = elements
+                        }
+                    }
+
+                    numberStr = ""
+                }
+            }
+        }
+
+        val result = map.filter { it.value.size >= 2 }.values
+
+        return result.sumOf { list -> list.sumOf { it } }
+    }
+
 }
+
+data class Point(val x: Int, val y: Int)
 
 fun safeExecute(block: () -> Unit) {
     try {
@@ -60,6 +167,6 @@ fun main() {
 ......755.
 ...$.*....
 .664.598..
-        """.trimIndent()(part1 = 4361)
+        """.trimIndent()(part1 = 4361, part2 = 467835)
     }
 }
